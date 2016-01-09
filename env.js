68e5a854038203a4969f1222b42710318ab3a914
onehunnid <climbrick1989@gmail.com>
2016-01-09 15:00:12 -0500

    adjusting formatting

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 // Add your Twitter Developer credentials here
 module.exports = {
-	TWITTER_CONSUMER_KEY : '',
+  TWITTER_CONSUMER_KEY : '',
   TWITTER_CONSUMER_SECRET : '',
   TWITTER_ACCESS_TOKEN_KEY : '',
   TWITTER_ACCESS_TOKEN_SECRET : ''
